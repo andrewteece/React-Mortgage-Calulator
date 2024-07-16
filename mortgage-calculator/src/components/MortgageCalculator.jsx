41208a207db4b1aa043aff09c3053f99b7babcc3
Andrew Teece <andrewteece@gmail.com>
2024-07-16 18:42:56 -0500

    mortgageCalculator styles

--- mortgage-calculator/src/components/MortgageCalculator.jsx
+++ mortgage-calculator/src/components/MortgageCalculator.jsx
@@ -1,4 +1,54 @@
+import { useState } from 'react';
+import './MortgageCalculator.css';
+
+const initialState = {
+  amount: '',
+  downPayment: '',
+  interestRate: '',
+  loanTerm: '',
+};
+
 function MortgageCalculator() {
+  const [formData, setFormData] = useState(initialState);
+  const [payment, setPayment] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [interes, setInterest] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    calculateMonthlyPayment();
+  };
+
+  const calculateMonthlyPayment = () => {
+    const { amount, downPayment, interestRate, loanTerm } = formData;
+
+    const loanAmount =
+      parseFloat(amount) - (downPayment ? parseFloat(downPayment) : 0);
+
+    const r = parseFloat(interestRate) / 100 / 12;
+    const n = parseFloat(loanTerm) * 12;
+
+    const numerator = loanAmount * r * Math.pow(1 + r, n);
+    const denumerator = Math.pow(1 + r, n) - 1;
+
+    const monthlyPayment = (numerator / denumerator).toFixed(2);
+    setPayment(monthlyPayment);
+    const totalPayable = (monthlyPayment * n).toFixed(2);
+    setTotal(totalPayable);
+    const totalInterest = (totalPayable - loanAmount).toFixed(2);
+    setInterest(totalInterest);
+  };
+
   return (
     <div className="mortgage-wrapper">
       <h2>Mortgage Calculator</h2>
